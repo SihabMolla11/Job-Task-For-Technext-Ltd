749824801b6421585807677b55cfdfb8de4937d0
unknown <sihabuddinmolla11@gmail.com>
2023-10-23 13:02:09 +0600

    create header section

--- src/Layout/pages/Home/HeaderSection/HeaderSection.jsx
+++ src/Layout/pages/Home/HeaderSection/HeaderSection.jsx
@@ -1,17 +1,17 @@
 const HeaderSection = () => {
   return (
     <div className="mt-24">
-      <div className="text-end mb-4">
-        <input type="checkbox" name="" id="upcoming" />
-        <label htmlFor="upcoming" className="text-[#212529] ml-2">
-          Show upcoming only
-        </label>
-      </div>
-      <div className=" flex justify-between">
+      <div className=" flex flex-col md:flex-row justify-between md:items-end gap-6">
         <div>search</div>
-        <div className="flex gap-6">
-          <div>by launch Status</div>
-          <div>by launch Data</div>
+        <div className=" md:text-end">
+          <input type="checkbox" name="" id="upcoming" />
+          <label htmlFor="upcoming" className="text-[#212529] ml-2">
+            Show upcoming only
+          </label>
+          <div className="flex flex-col md:flex-row gap-4 mt-4">
+            <div>by launch Status</div>
+            <div>by launch Data</div>
+          </div>
         </div>
       </div>
     </div>
